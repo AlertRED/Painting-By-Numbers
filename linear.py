bca8781c2de237e220649d8d5f5458aa258c3ee3
groneveld <groneveld.co@gmail.com>
2020-07-17 00:44:12 +0700

    recounting for one polygon added

--- linear.py
+++ linear.py
@@ -1,11 +1,17 @@
 import copy
 import sys
 from timeit import default_timer as timer
-
 import cv2
 import numpy as np
 from mpi4py import MPI
 from skimage import io
+from enum import Enum
+
+
+class PixelStatus(Enum):
+    recounted = 0
+    too_small = 1
+    not_counted = 2
 
 
 def get_borders(N, commsize, rank):
@@ -35,13 +41,54 @@ def is_equal(list_1, list_2):
     return flag
 
 
-original_image = io.imread('img.jpg')
+def polygon_recount(image_matrix):
+    curr_x = curr_y = 0
+    min_limit = 50
+    current_polygon = []
+    current_color = image_matrix[curr_x][curr_y]
+    x_len = len(image_matrix)
+    y_len = len(image_matrix[0])
+    is_counted = [[PixelStatus.not_counted.value] * y_len for _ in range(x_len)]
+    while True:
+        curr_line = [(curr_x, curr_y)]
+        current_polygon.append((curr_x, curr_y))
+        #pixel_amount += 1
+        #is_counted[curr_x][curr_y] = False
+        while is_equal(current_color, image_matrix[curr_x][curr_y + 1]):
+            curr_y += 1
+            curr_line.append((curr_x, curr_y))
+            current_polygon.append((curr_x, curr_y))
+            #is_counted[curr_x][curr_y] = False
+            #pixel_amount += 1
+        if is_equal(current_color, image_matrix[curr_line[0][0] + 1][curr_line[0][1]]):
+            curr_x += 1
+            curr_y = curr_line[0][1]
+            while curr_y - 1 > 0 and is_equal(current_color, image_matrix[curr_x][curr_y - 1]):
+                curr_y -= 1
+        else:
+            for x, y in curr_line:
+                x += 1
+                if is_equal(current_color, image_matrix[x][y]):
+                    curr_x = x
+                    curr_y = y
+            else:
+                break
+    if len(current_polygon) >= min_limit:
+        for x,y in current_polygon:
+            is_counted[x][y] = PixelStatus.recounted.value
+    else:
+        for x,y in current_polygon:
+            is_counted[x][y] = PixelStatus.too_small.value
+
+
+
+original_image = io.imread('img_2.jpg')
 
 scale_percent = 60
-#width = int(original_image.shape[1] * scale_percent / 100)
-#height = int(original_image.shape[0] * scale_percent / 100)
-width = 200
-height = 200
+# width = int(original_image.shape[1] * scale_percent / 100)
+# height = int(original_image.shape[0] * scale_percent / 100)
+width = 100
+height = 100
 dimensions = (width, height)
 
 resized = cv2.resize(original_image, dimensions, interpolation=cv2.INTER_AREA)
@@ -49,7 +96,7 @@ resized = cv2.resize(original_image, dimensions, interpolation=cv2.INTER_AREA)
 rgba_image = cv2.cvtColor(resized, cv2.COLOR_RGB2RGBA)
 
 # количество кластеров
-N = 20
+N = 10
 # первичная инициализация центров кластеров рандомными значениями
 cluster_centers = []
 for i in range(0, N):
@@ -125,32 +172,33 @@ final_img = [[[0] * 4 for _ in range(dimensions[0])] for _ in range(dimensions[1
 black = np.array([0, 0, 0, 255]).astype(np.uint8)
 white = np.array([255, 255, 255, 255]).astype(np.uint8)
 
+polygon_recount(new_img)
 # контурирование
-for x in range(height):
-    for y in range(width):
-        # если не крайний столбец и не крайняя строка
-        if x != (height - 1) and y != (width - 1):
-            if not is_equal(new_img[x][y], new_img[x][y + 1]) \
-                    or not is_equal(new_img[x][y], new_img[x + 1][y]):
-                final_img[x][y] = black
-            else:
-                final_img[x][y] = white
-        elif x == (height - 1) and y != (width - 1):
-            if not is_equal(new_img[x][y], new_img[x][y + 1]):
-                final_img[x][y] = black
-            else:
-                final_img[x][y] = white
-        elif y == (width - 1) and x != (height - 1):
-            if not is_equal(new_img[x][y], new_img[x + 1][y]):
-                final_img[x][y] = black
-            else:
-                final_img[x][y] = white
-        else:
-            final_img[x][y] = white
+# for x in range(height):
+#     for y in range(width):
+#         # если не крайний столбец и не крайняя строка
+#         if x != (height - 1) and y != (width - 1):
+#             if not is_equal(new_img[x][y], new_img[x][y + 1]) \
+#                     or not is_equal(new_img[x][y], new_img[x + 1][y]):
+#                 final_img[x][y] = black
+#             else:
+#                 final_img[x][y] = white
+#         elif x == (height - 1) and y != (width - 1):
+#             if not is_equal(new_img[x][y], new_img[x][y + 1]):
+#                 final_img[x][y] = black
+#             else:
+#                 final_img[x][y] = white
+#         elif y == (width - 1) and x != (height - 1):
+#             if not is_equal(new_img[x][y], new_img[x + 1][y]):
+#                 final_img[x][y] = black
+#             else:
+#                 final_img[x][y] = white
+#         else:
+#             final_img[x][y] = white
 
-out = cv2.cvtColor(np.asarray(final_img), cv2.COLOR_RGBA2BGR)
+# out = cv2.cvtColor(np.asarray(final_img), cv2.COLOR_RGBA2BGR)
 
-#cv2.imshow("cont", out)
-#cv2.imshow("dv", output)
+# cv2.imshow("cont", out)
+# cv2.imshow("dv", output)
 
 cv2.waitKey(0)

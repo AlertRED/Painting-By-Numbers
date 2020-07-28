fc99af444d5cd5ac1e59d790695d9eee631cc657
groneveld <groneveld.co@gmail.com>
2020-07-28 17:57:22 +0700

    Polygon Merging added

--- linear.py
+++ linear.py
@@ -1,5 +1,6 @@
 import copy
 import sys
+import math
 from timeit import default_timer as timer
 import cv2
 import numpy as np
@@ -91,7 +92,8 @@ def polygon_recount(image_matrix):
                 curr_y += 1
                 curr_line.append((curr_x, curr_y))
                 current_polygon.append((curr_x, curr_y))
-            if curr_line[0][0] + 1 < x_len - 1 and is_equal(current_color, image_matrix[curr_line[0][0] + 1][curr_line[0][1]]):
+            if curr_line[0][0] + 1 < x_len - 1 and is_equal(current_color,
+                                                            image_matrix[curr_line[0][0] + 1][curr_line[0][1]]):
                 curr_x += 1
                 curr_y = curr_line[0][1]
                 while curr_y - 1 > 0 and is_equal(current_color, image_matrix[curr_x][curr_y - 1]):
@@ -113,25 +115,105 @@ def polygon_recount(image_matrix):
                 is_counted[x][y] = PolygonStatus.too_small.value
         curr_x, curr_y = get_new_start(current_polygon, y_len, x_len, is_counted)
         current_polygon = []
-    print(is_counted)
+    return is_counted
 
+
+def pixel_difference(center, pixel):
+    center = center.astype(np.int32)
+    pixel = pixel.astype(np.int32)
+    distance = ((center[0] - pixel[0]) ** 2
+                + (center[1] - pixel[1]) ** 2
+                + (center[2] - pixel[2]) ** 2
+                + (center[3] - pixel[3]) ** 2) ** (1 / 2)
+    return distance
+
+
+def polygon_merge(image_matrix, polygon_status):
+    x_len = len(image_matrix)
+    y_len = len(image_matrix[0])
+    for x in range(x_len):
+        for y in range(y_len):
+            if polygon_status[x][y] == PolygonStatus.too_small.value:
+                min_distance = sys.maxsize
+                color_difference = 0
+                new_color = []
+                if y > 0:
+                    color_difference = pixel_difference(image_matrix[x][y], image_matrix[x][y - 1])
+                    if color_difference < min_distance:
+                        min_distance = color_difference
+                        new_color = image_matrix[x][y - 1]
+                if x > 0:
+                    color_difference = pixel_difference(image_matrix[x][y], image_matrix[x - 1][y])
+                    if color_difference < min_distance:
+                        min_distance = color_difference
+                        new_color = image_matrix[x - 1][y]
+                if y < y_len - 1:
+                    if polygon_status[x][y + 1] != PolygonStatus.too_small.value:
+                        color_difference = pixel_difference(image_matrix[x][y], image_matrix[x][y + 1])
+                        if color_difference < min_distance:
+                            min_distance = color_difference
+                            new_color = image_matrix[x][y + 1]
+                if x < x_len - 1:
+                    if polygon_status[x + 1][y] != PolygonStatus.too_small.value:
+                        color_difference = pixel_difference(image_matrix[x][y], image_matrix[x + 1][y])
+                        if color_difference < min_distance:
+                            min_distance = color_difference
+                            new_color = image_matrix[x + 1][y]
+                image_matrix[x][y] = new_color
+                polygon_status[x][y] = PolygonStatus.recounted.value
+    return image_matrix
+
+
+def contouring(image_matrix):
+    final_img = [[[0] * 4 for _ in range(dimensions[0])] for _ in range(dimensions[1])]
+    black = np.array([0, 0, 0, 255]).astype(np.uint8)
+    white = np.array([255, 255, 255, 255]).astype(np.uint8)
+    x_len = len(image_matrix)
+    y_len = len(image_matrix[0])
+    for x in range(x_len):
+        for y in range(y_len):
+            if x != (x_len - 1) and y != (y_len - 1):
+                if not is_equal(image_matrix[x][y], image_matrix[x][y + 1]) \
+                        or not is_equal(image_matrix[x][y], image_matrix[x + 1][y]):
+                    final_img[x][y] = black
+                else:
+                    final_img[x][y] = white
+            elif x == (height - 1) and y != (width - 1):
+                if not is_equal(image_matrix[x][y], image_matrix[x][y + 1]):
+                    final_img[x][y] = black
+                else:
+                    final_img[x][y] = white
+            elif y == (width - 1) and x != (height - 1):
+                if not is_equal(image_matrix[x][y], image_matrix[x + 1][y]):
+                    final_img[x][y] = black
+                else:
+                    final_img[x][y] = white
+            else:
+                final_img[x][y] = white
+    return final_img
 
 
 original_image = io.imread('img_2.jpg')
 
-scale_percent = 60
-# width = int(original_image.shape[1] * scale_percent / 100)
-# height = int(original_image.shape[0] * scale_percent / 100)
-width = 100
-height = 100
+max_dimension = 500
+# original_dimensions = [x,y]
+original_dimensions = [original_image.shape[0], original_image.shape[1]]
+if max(original_dimensions[0], original_dimensions[1]) == original_image.shape[0]:
+    height = max_dimension
+    scale_percent = int(max_dimension / (original_image.shape[0] / 100))
+    width = int(original_image.shape[1] * scale_percent / 100)
+else:
+    width = max_dimension
+    scale_percent = int(max_dimension / (original_image.shape[1] / 100))
+    height = int(original_image.shape[0] * scale_percent / 100)
+
 dimensions = (width, height)
 
 resized = cv2.resize(original_image, dimensions, interpolation=cv2.INTER_AREA)
-
 rgba_image = cv2.cvtColor(resized, cv2.COLOR_RGB2RGBA)
 
 # количество кластеров
-N = 10
+N = math.ceil(max(width, height) / 100 * 8)
 # первичная инициализация центров кластеров рандомными значениями
 cluster_centers = []
 for i in range(0, N):
@@ -163,10 +245,7 @@ while (True):
             cluster_id = 0
             for i in range(0, N):
                 current_center = cluster_centers[i].astype(np.int32)
-                distance = ((current_center[0] - pixel[0]) ** 2
-                            + (current_center[1] - pixel[1]) ** 2
-                            + (current_center[2] - pixel[2]) ** 2
-                            + (current_center[3] - pixel[3]) ** 2) ** (1 / 2)
+                distance = pixel_difference(current_center, pixel)
                 if distance < min_distance:
                     min_distance = distance
                     cluster_id = i
@@ -200,39 +279,17 @@ while (True):
         break
 end = timer()
 print('Time - ', end - start)
-output = cv2.cvtColor(np.asarray(new_img), cv2.COLOR_RGBA2BGR)
-imgray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
 
-final_img = [[[0] * 4 for _ in range(dimensions[0])] for _ in range(dimensions[1])]
-black = np.array([0, 0, 0, 255]).astype(np.uint8)
-white = np.array([255, 255, 255, 255]).astype(np.uint8)
-polygon_recount(new_img)
+polygon_status = polygon_recount(new_img)
+image_recounted = polygon_merge(new_img, polygon_status)
+
+output = cv2.cvtColor(np.asarray(image_recounted), cv2.COLOR_RGBA2BGR)
+
 # контурирование
-# for x in range(height):
-#     for y in range(width):
-#         # если не крайний столбец и не крайняя строка
-#         if x != (height - 1) and y != (width - 1):
-#             if not is_equal(new_img[x][y], new_img[x][y + 1]) \
-#                     or not is_equal(new_img[x][y], new_img[x + 1][y]):
-#                 final_img[x][y] = black
-#             else:
-#                 final_img[x][y] = white
-#         elif x == (height - 1) and y != (width - 1):
-#             if not is_equal(new_img[x][y], new_img[x][y + 1]):
-#                 final_img[x][y] = black
-#             else:
-#                 final_img[x][y] = white
-#         elif y == (width - 1) and x != (height - 1):
-#             if not is_equal(new_img[x][y], new_img[x + 1][y]):
-#                 final_img[x][y] = black
-#             else:
-#                 final_img[x][y] = white
-#         else:
-#             final_img[x][y] = white
+contours = contouring(image_recounted)
+out = cv2.cvtColor(np.asarray(contours), cv2.COLOR_RGBA2BGR)
 
-# out = cv2.cvtColor(np.asarray(final_img), cv2.COLOR_RGBA2BGR)
-
-# cv2.imshow("cont", out)
-# cv2.imshow("dv", output)
+cv2.imshow("cont", out)
+cv2.imshow("dv", output)
 
 cv2.waitKey(0)
